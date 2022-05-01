fd489305dd8163e89e95dbd50e7a30c4e4e45812
Andrey Barreto <dey@LAPTOP-RVOBPJ62.localdomain>
2022-05-01 18:02:14 -0300

    feature/scripts in node modules & singin

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,8 @@
-import { Injectable } from '@nestjs/common';
+import {
+  BadRequestException,
+  ForbiddenException,
+  Injectable,
+} from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { AuthDto } from './dto';
 import * as argon from 'argon2';
@@ -8,6 +12,19 @@ export class AuthService {
   constructor(private prisma: PrismaService) {}
   async signup(dto: AuthDto) {
     const hash = await argon.hash(dto.password);
+
+    const emailExist =
+      await this.prisma.user.findUnique({
+        where: {
+          email: dto.email,
+        },
+      });
+
+    if (emailExist) {
+      throw new BadRequestException(
+        'Email already in use',
+      );
+    }
 
     const user = await this.prisma.user.create({
       data: {
@@ -24,7 +41,30 @@ export class AuthService {
     return user;
   }
 
-  signin() {
-    return { msg: 'I am signin' };
+  async signin(dto: AuthDto) {
+    const user =
+      await this.prisma.user.findUnique({
+        where: {
+          email: dto.email,
+        },
+      });
+
+    if (!user) {
+      throw new ForbiddenException(
+        'Credentials incorrect',
+      );
+    }
+    const pwMatchs = await argon.verify(
+      user.hash,
+      dto.password,
+    );
+
+    if (!pwMatchs) {
+      throw new ForbiddenException(
+        'Credentials incorrect',
+      );
+    }
+    delete user.hash;
+    return user;
   }
 }
