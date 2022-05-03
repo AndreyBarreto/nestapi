a84c046c2bd0b3b5f7e7b3f748ba3d6552e99186
Andrey Barreto <dey@LAPTOP-RVOBPJ62.localdomain>
2022-05-03 20:30:00 -0300

    feature/jwt validation

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,15 +1,18 @@
 import {
   Controller,
   Get,
+  Req,
   UseGuards,
 } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
+import { Request } from 'express';
 
 @Controller('user')
 export class UserController {
-  @UseGuards(AuthGuard('jwt'))
+  @UseGuards(AuthGuard('jwtauth'))
   @Get('me')
-  getme() {
+  getme(@Req() req: Request) {
+    console.log(req.user);
     return 'user info';
   }
 }
