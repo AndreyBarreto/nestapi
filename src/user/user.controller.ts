0d360fb7c5337edd74a14459cdd6ee38e4429a87
Andrey Barreto <dey@LAPTOP-RVOBPJ62.localdomain>
2022-05-03 21:14:10 -0300

    feature/custom decorators

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,18 +1,18 @@
 import {
   Controller,
   Get,
-  Req,
   UseGuards,
 } from '@nestjs/common';
-import { AuthGuard } from '@nestjs/passport';
-import { Request } from 'express';
+import { User } from '@prisma/client';
+import { GetUser } from 'src/auth/decorator';
+import { JwtGuard } from 'src/auth/guard';
 
+@UseGuards(JwtGuard)
 @Controller('user')
 export class UserController {
-  @UseGuards(AuthGuard('jwtauth'))
   @Get('me')
-  getme(@Req() req: Request) {
-    console.log(req.user);
+  getme(@GetUser('') user: User) {
+    console.log(user);
     return 'user info';
   }
 }
